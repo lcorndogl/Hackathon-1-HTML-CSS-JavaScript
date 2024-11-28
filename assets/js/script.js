87deb22f929b6d38b8854567b81c6d0cc759c345
Michael Cornall <mik.reds.forever@gmail.com>
2024-11-28 09:32:06 +0000

    Add function skeletons

---
+++ assets/js/script.js
@@ -0,0 +1,25 @@
+
+
+
+
+function startGame() {
+    const question = getQuestion();
+    const answer = getAnswer(question);
+
+}
+
+function getQuestion() {
+
+}
+
+function getAnswer(question) {
+
+}
+
+function makeGuess() {
+    
+}
+
+function cheat() {
+
+}
